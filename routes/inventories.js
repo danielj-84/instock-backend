92be6877834674e61f42e6b10eb8b902a86e4371
danielj-84 <danieldimatteo123@gmail.com>
2022-06-13 19:20:01 -0400

    post request functional, including warehouseId - requires validation

--- routes/inventories.js
+++ routes/inventories.js
@@ -1,17 +1,47 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const fs = require("fs");
+const { v4: uuid } = require("uuid");
 
 //get vids from JSON - parse for front-end use
 const getInv = () => {
-    const invList = fs.readFileSync("./data/inventories.json");
-    return JSON.parse(invList);
-  };
-  
-  //write vids to JSON - stringify for back-end storage
-  const addToInv = (updatedInv) => {
-    fs.writeFileSync("./data/inventories.json", JSON.stringify(updatedInv));
+  const invList = fs.readFileSync("./data/inventories.json");
+  return JSON.parse(invList);
+};
+
+//write inv to JSON - stringify for back-end storage
+const addToInv = (updatedInv) => {
+  fs.writeFileSync("./data/inventories.json", JSON.stringify(updatedInv));
+};
+
+//get warehouse ID (for POST-new item)
+const warehouseID = (props) => {
+  let warehouseList = JSON.parse(fs.readFileSync("./data/warehouses.json"));
+  const wareId = warehouseList.find(warehouse => warehouse.name === props.warehouseName).id;
+  return wareId;
+}
+
+//POST new inventory item
+router.post("/", (req, res) => {
+  const data = req.body;
+
+  const newItem = {
+    id: uuid(),
+    warehouseID: warehouseID(data),
+    warehouseName: data.warehouseName,
+    itemName: data.itemName,
+    description: data.description,
+    category: data.category,
+    status: data.status,
+    quantity: data.quantity
   };
 
+  let updatedItems = getInv();
+  updatedItems.push(newItem);
+
+  // addToInv(updatedItems);
+
+  res.status(200).send(newItem);
+})
 
 module.exports = router;
