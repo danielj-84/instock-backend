98974611c05d9917dcf6f6f869254d2c6f528801
danielj-84 <danieldimatteo123@gmail.com>
2022-06-13 17:29:12 -0400

    added and tested get-all-warehouses route

--- routes/warehouses.js
+++ routes/warehouses.js
@@ -1,16 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 
 //get vids from JSON - parse for front-end use
 const getWare = () => {
-    const wareList = fs.readFileSync("./data/warehouses.json");
-    return JSON.parse(invList);
-  };
-  
-  //write vids to JSON - stringify for back-end storage
-  const addToWare = (updatedWare) => {
-    fs.writeFileSync("./data/warehouses.json", JSON.stringify(updatedWare));
-  };
+  const wareList = fs.readFileSync("./data/warehouses.json");
+  return JSON.parse(wareList);
+};
+
+//write vids to JSON - stringify for back-end storage
+const addToWare = (updatedWare) => {
+  fs.writeFileSync("./data/warehouses.json", JSON.stringify(updatedWare));
+};
+
+//GET all warehouse info from JSON file
+router.get("/", (_req, res) => {
+  let warehouses = getWare().map((warehouse) => {
+    return {
+      id: warehouse.id,
+      name: warehouse.name,
+      address: warehouse.address,
+      city: warehouse.city,
+      country: warehouse.country,
+      contact: {
+        name: warehouse.contact.name,
+        position: warehouse.contact.position,
+        phone: warehouse.contact.phone,
+        email: warehouse.contact.email,
+      },
+    };
+  });
+  res.status(200).json(warehouses);
+});
 
 module.exports = router;
