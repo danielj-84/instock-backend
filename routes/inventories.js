21680fe0a1e9fb87b77e6e586fcf3ba5f682dbe8
danielj-84 <danieldimatteo123@gmail.com>
2022-06-13 16:25:44 -0400

    initial file and folder structure

---
+++ routes/inventories.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const fs = require("fs");
+
+//get vids from JSON - parse for front-end use
+const getInv = () => {
+    const invList = fs.readFileSync("./data/videos.json");
+    return JSON.parse(invList);
+  };
+  
+  //write vids to JSON - stringify for back-end storage
+  const addToInv = (updatedInv) => {
+    fs.writeFileSync("./data/inventories.json", JSON.stringify(updatedInv));
+  };
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+//config
+require("dotenv").config();
+const PORT = process.env.PORT || 8080;
+
+// middelware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cors());
+
+//routes
+const inventoriesRoutes = require("./routes/inventories");
+app.use("/inventories", inventoriesRoutes);
+
+const warehousesRoutes = require("./routes/warehouses");
+app.use("/warehouses", warehousesRoutes);
+
+//server startup
+app.listen(PORT, () => {
+    console.log(`App is running on PORT ${PORT}`)
+})
